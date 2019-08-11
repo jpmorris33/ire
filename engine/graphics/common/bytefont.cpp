a4fc7b989d7d165f9ff26dd19e76d5b579626e5e
jpmorris <jpmorris@Pettersohn.(none)>
2019-08-11 21:46:00 +0100

    fix(graphics) - Fix crash when rendering fonts with an unsupported character.  This worked by fluke in C, but crashed the assembler font routines in windows

--- engine/graphics/common/bytefont.cpp
+++ engine/graphics/common/bytefont.cpp
@@ -32,14 +32,12 @@ static void draw_colourfont_32c(unsigned char *buffer, unsigned char *fontdata, 
 extern "C" void draw_colourfont_16asm(unsigned char *buffer, unsigned char *fontdata, unsigned int *pal, int w, int h, int offset);
 extern "C" void draw_colourfont_32asm(unsigned char *buffer, unsigned char *fontdata, unsigned int *pal, int w, int h, int offset);
 
-struct FONTPACKET
-	{
+struct FONTPACKET {
 	int w,h;
 	unsigned char *data;
-	};
+};
 
-class BYTEFONT : IREFONT
-	{
+class BYTEFONT : IREFONT {
 	public:
 		BYTEFONT();
 		BYTEFONT(const char *filename);
@@ -53,15 +51,14 @@ class BYTEFONT : IREFONT
 		FONTPACKET fontchar[256];
 		unsigned int pal[256];
 		int fw,fh,colourfont;
-	};
+};
 
 
 //
 //  Bootstrap
 //
 
-IREFONT *MakeBytefont()
-{
+IREFONT *MakeBytefont() {
 if(!draw_bytefont_func)
 	InitByteFont();
 
@@ -70,8 +67,7 @@ return (IREFONT *)fnt;
 }
 
 	
-IREFONT *MakeBytefont(const char *filename)
-{
+IREFONT *MakeBytefont(const char *filename) {
 if(!draw_bytefont_func)
 	InitByteFont();
 
@@ -86,8 +82,7 @@ return (IREFONT *)fnt;
 //
 
 
-BYTEFONT::BYTEFONT() : IREFONT()
-{
+BYTEFONT::BYTEFONT() : IREFONT() {
 // Create invalid font
 fw=0;
 fh=0;
@@ -97,8 +92,7 @@ colourfont=0;
 }
 
 
-BYTEFONT::BYTEFONT(const char *filename) : IREFONT(filename)
-{
+BYTEFONT::BYTEFONT(const char *filename) : IREFONT(filename) {
 int len,ctr;
 unsigned char r,g,b;
 IRECOLOUR packcol;
@@ -111,11 +105,10 @@ memset(fontchar,0,sizeof(fontchar));
 memset(pal,0,sizeof(pal));
 colourfont=0;
 
-if(!filename)
-	{
+if(!filename) {
 	printf("IREFONT: no filename\n");
 	return;
-	}
+}
 
 fp = iopen(filename);
 
@@ -123,14 +116,12 @@ unsigned int header=0;
 header=igetl_i(fp);
 if(header == IRE_FNT2)
 	colourfont=1;
-if(header != IRE_FNT1 && header != IRE_FNT2)
-	{
+if(header != IRE_FNT1 && header != IRE_FNT2) {
 	printf("BYTEFONT: Wrong format\n");
 	return;
-	}
+}
 
-for(ctr=0;ctr<256;ctr++)
-	{
+for(ctr=0;ctr<256;ctr++) {
 	fontchar[ctr].w=igetsh_i(fp);
 	fontchar[ctr].h=igetsh_i(fp);
 	if(fontchar[ctr].h > fh)
@@ -139,12 +130,11 @@ for(ctr=0;ctr<256;ctr++)
 		fw=fontchar[ctr].w;
 
 	len=fontchar[ctr].w * fontchar[ctr].h;
-	if(len > 0)
-		{
+	if(len > 0) {
 		fontchar[ctr].data = (unsigned char *)M_get(1,len);
 		iread(fontchar[ctr].data,len,fp);
-		}
 	}
+}
 
 // If we have a palette, read it in and convert it to native BPP format
 if(colourfont)
@@ -161,8 +151,7 @@ iclose(fp);
 }
 
 
-BYTEFONT::~BYTEFONT()
-{
+BYTEFONT::~BYTEFONT() {
 for(int ctr=0;ctr<256;ctr++)
 	if(fontchar[ctr].data)
 		M_free(fontchar[ctr].data);
@@ -172,8 +161,7 @@ fh=0;
 }
 
 
-void BYTEFONT::Draw(IREBITMAP *dest, int x, int y, IRECOLOUR *fg, IRECOLOUR *bg, const char *string)
-{
+void BYTEFONT::Draw(IREBITMAP *dest, int x, int y, IRECOLOUR *fg, IRECOLOUR *bg, const char *string) {
 int fgcol=-1;
 int bgcol=-1;
 int w,h,c;
@@ -207,37 +195,26 @@ y <<= shiftfactor; // convert to appropriate BPP
 buffer += y;
 
 
-if(colourfont)
-	{
-	for(;*string;string++)
-		{
-		c=(*string) & 0xff;
-		ch=&fontchar[c];
-		if(x+ch->w >= w)
-			break;
-		draw_colourfont_func(buffer,ch->data,pal,ch->w,ch->h,w-ch->w);
-		x+=ch->w;
-		buffer += (ch->w << shiftfactor);  // Shuffle buffer along
+for(;*string;string++) {
+	c=(*string) & 0xff;
+	ch=&fontchar[c];
+	if(x+ch->w >= w) {
+		break;
+	}
+
+	if(ch->data) {
+		if(colourfont) {
+			draw_colourfont_func(buffer,ch->data,pal,ch->w,ch->h,w-ch->w);
+		} else {
+			draw_bytefont_func(buffer,ch->data,ch->w,ch->h,w-ch->w,fgcol);
 		}
 	}
-else
-	{
-	for(;*string;string++)
-		{
-		c=(*string) & 0xff;
-		ch=&fontchar[c];
-		if(x+ch->w >= w)
-			break;
-		draw_bytefont_func(buffer,ch->data,ch->w,ch->h,w-ch->w,fgcol);
-		x+=ch->w;
-		buffer += (ch->w << shiftfactor);
-		}
-	}
+	x+=ch->w;
+	buffer += (ch->w << shiftfactor);  // Shuffle buffer along
+}
 }
 
-
-void BYTEFONT::Printf(IREBITMAP *dest, int x, int y, IRECOLOUR *fg, IRECOLOUR *bg, const char *fmt, ...)
-{
+void BYTEFONT::Printf(IREBITMAP *dest, int x, int y, IRECOLOUR *fg, IRECOLOUR *bg, const char *fmt, ...) {
 char linebuf[1024];
 va_list ap;
 
@@ -254,30 +231,26 @@ va_end(ap);
 }
 
 
-int BYTEFONT::Length(const char *str)
-{
+int BYTEFONT::Length(const char *str) {
 int w,c;
 if(!str)
 	return 0;
 w=0;
-for(;*str;str++)
-	{
+for(;*str;str++) {
 	c=(*str)&0xff;
 	w+=fontchar[c].w;
-	}
+}
 
 return w;
 }
 
 
-int BYTEFONT::Height()
-{
+int BYTEFONT::Height() {
 return fh;
 }
 
 
-int BYTEFONT::IsColoured()
-{
+int BYTEFONT::IsColoured() {
 return colourfont;
 }
 
@@ -285,19 +258,17 @@ return colourfont;
 ///////////
 ///////////
 
-void InitByteFont()
-{
+void InitByteFont() {
 switch(_IRE_BPP) {
 	case 15:
 	case 16:
 		draw_bytefont_func=draw_bytefont_16c;
 		draw_colourfont_func=draw_colourfont_16c;
 #ifndef NO_ASM
-		if(!ire_NOASM)
-			{
+		if(!ire_NOASM) {
 			draw_bytefont_func=draw_bytefont_16asm;
 			draw_colourfont_func=draw_colourfont_16asm;
-			}
+		}
 #endif
 		shiftfactor=1;
 		break;
@@ -305,94 +276,81 @@ switch(_IRE_BPP) {
 		draw_bytefont_func=draw_bytefont_32c;
 		draw_colourfont_func=draw_colourfont_32c;
 #ifndef NO_ASM
-		if(!ire_NOASM)
-			{
+		if(!ire_NOASM) {
 			draw_bytefont_func=draw_bytefont_32asm;
 			draw_colourfont_func=draw_colourfont_32asm;
-			}
+		}
 #endif
 		
 		shiftfactor=2;
 		break;
-	}
+}
 }
 
 //
 // C rendering backends
 //
 
-void draw_bytefont_16c(unsigned char *buffer, unsigned char *fontdata, int w, int h, int offset, unsigned int colour)
-{
+void draw_bytefont_16c(unsigned char *buffer, unsigned char *fontdata, int w, int h, int offset, unsigned int colour) {
 uint16_t *buffer16 = (uint16_t *)buffer;
 int yctr,xctr;
 
-for(yctr=0;yctr<h;yctr++)
-	{
-	for(xctr=0;xctr<w;xctr++)
-		{
+for(yctr=0;yctr<h;yctr++) {
+	for(xctr=0;xctr<w;xctr++) {
 		if(*fontdata++)
 			*buffer16=colour;
 		buffer16++;
-		}
-	buffer16 +=offset;
 	}
+	buffer16 +=offset;
+}
 }
 
 
-void draw_bytefont_32c(unsigned char *buffer, unsigned char *fontdata, int w, int h, int offset, unsigned int colour)
-{
+void draw_bytefont_32c(unsigned char *buffer, unsigned char *fontdata, int w, int h, int offset, unsigned int colour) {
 uint32_t *buffer32 = (uint32_t *)buffer;
 int yctr,xctr;
 
-for(yctr=0;yctr<h;yctr++)
-	{
-	for(xctr=0;xctr<w;xctr++)
-		{
+for(yctr=0;yctr<h;yctr++) {
+	for(xctr=0;xctr<w;xctr++) {
 		if(*fontdata++)
 			*buffer32=colour;
 		buffer32++;
-		}
-	buffer32 +=offset;
 	}
+	buffer32 +=offset;
+}
 }
 
 
-void draw_colourfont_16c(unsigned char *buffer, unsigned char *fontdata, unsigned int *pal, int w, int h, int offset)
-{
+void draw_colourfont_16c(unsigned char *buffer, unsigned char *fontdata, unsigned int *pal, int w, int h, int offset) {
 uint16_t *buffer16 = (uint16_t *)buffer;
 int yctr,xctr;
 unsigned char c;
 
-for(yctr=0;yctr<h;yctr++)
-	{
-	for(xctr=0;xctr<w;xctr++)
-		{
+for(yctr=0;yctr<h;yctr++) {
+	for(xctr=0;xctr<w;xctr++) {
 		c=*fontdata++;
 		if(c)
 			*buffer16=(uint16_t)pal[c];
 		buffer16++;
-		}
-	buffer16 +=offset;
 	}
+	buffer16 +=offset;
+}
 }
 
 
-void draw_colourfont_32c(unsigned char *buffer, unsigned char *fontdata, unsigned int *pal, int w, int h, int offset)
-{
+void draw_colourfont_32c(unsigned char *buffer, unsigned char *fontdata, unsigned int *pal, int w, int h, int offset) {
 uint32_t *buffer32 = (uint32_t *)buffer;
 int yctr,xctr;
 unsigned char c;
 
-for(yctr=0;yctr<h;yctr++)
-	{
-	for(xctr=0;xctr<w;xctr++)
-		{
+for(yctr=0;yctr<h;yctr++) {
+	for(xctr=0;xctr<w;xctr++) {
 		c=*fontdata++;
 		if(c)
 			*buffer32=pal[c];
 		buffer32++;
-		}
-	buffer32 +=offset;
 	}
+	buffer32 +=offset;
+}
 }
 
